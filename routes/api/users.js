2bd44dbba72a9841058d2e898a6f767b43a4b140
BAILLY <stephen.bailly@capgemini.com>
2023-03-28 14:35:10 +0200

    Backend register & login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,66 +1,82 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require('config');
 const { check, validationResult } = require("express-validator");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
-// @route  GET api/users
+// @route  POST api/users
 // @desc   Register user
 // @access Public
 router.post(
   "/",
   [
     check("name", "Name is required").not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please insert a password with 8 or more characters').isLength({ min: 8 })
-],
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please insert a password with 8 or more characters"
+    ).isLength({ min: 8 }),
+  ],
   async (req, res) => {
-   const errors = validationResult(req);
-   if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-   }
-
-   const { name, email, password } = req.body;
-
-   try {
-    // See if user exists
-    let user = await User.findOne({ email });
-
-    if(user) {
-        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-   // Get users gravatar
-   const avatar = gravatar.url(email, {
-    s: '200',
-    r: 'pg',
-    d: 'mm'
-   });
+    const { name, email, password } = req.body;
 
-   user = new User({
-    name,
-    email,
-    avatar,
-    password
-   });
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
 
-   // Encrypt password
-   const salt = await bcrypt.genSalt(10);
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
 
-   user.password = await bcrypt.hash(password, salt);
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
 
-   await user.save();
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-   // Return jsonwebtoken
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
 
-   res.send("User registered");
+      user.password = await bcrypt.hash(password, salt);
 
-   } catch(err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-   }   
+      await user.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'),
+      { expiresIn: 360000 }, (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      });
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
